bde8b54d85ceae013c0821e05f39a4c61b7ed18e
Denis R <96629363+NedSaw@users.noreply.github.com>
2022-04-14 11:24:41 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 deposit = []
-money_str = input("введите сумму:")
+money_str = input("Введите сумму:")
 money = float(money_str)
+
 for key in per_cent:
     deposit.append(money*per_cent[key]/100)
-    print(key,"процент по депозитам = ",deposit[-1])
-    print("максимальный депозит =",max(deposit))
+    print(key,"Процент по депозитам = ",deposit[-1])
+    print("Максимальная сумма, которую вы можете заработать =",max(deposit))
